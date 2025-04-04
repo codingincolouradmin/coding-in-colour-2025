3eec1c12b7e31d29b9834ba582fd1c1e9787ed9b
goofyangel <aleemertariq@gmail.com>
2025-04-04 15:41:54 -0700

    Added console logs to test the modulo operation.

--- milestone3/ui-tree/src/InspirationGenerator.jsx
+++ milestone3/ui-tree/src/InspirationGenerator.jsx
@@ -4,14 +4,21 @@ import FancyText from './FancyText';
 
 export default function InspirationGenerator({children}) {
   const [index, setIndex] = React.useState(0);
+  const [clicked, setClicked] = React.useState(0);
   const quote = quotes[index];
-  const next = () => setIndex((index + 1) % quotes.length);
+
+  const handleClick = () => {
+    console.log(`index is ${index}`);
+    console.log(`current click is ${clicked}`)
+    setIndex((index + 1) % quotes.length)
+    setClicked(clicked + 1)
+  }
 
   return (
     <>
       <p>Your inspirational quote is:</p>
       <FancyText text={quote} />
-      <button onClick={next}>Inspire me again</button>
+      <button onClick={handleClick}>Inspire me again</button>
       {children}
     </>
   );
