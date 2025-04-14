084b3b503f7eadc4452206c958ea4d74e01be4b7
goofyangel <aleemertariq@gmail.com>
2025-04-14 10:35:33 -0700

    Implemented Part 1 of Phonebook.

--- milestone3/phonebook/src/App.jsx
+++ milestone3/phonebook/src/App.jsx
@@ -1,7 +1,42 @@
+import { useState } from 'react'
+
 const App = () => {
+  const [ persons, setPersons ] = useState([
+    'Arto Hellas',
+    'Ada Lovelace',
+    'Dan Abramov',
+    'Mary Poppendieck'
+  ])
+  const [ name, setName ] = useState('')
+
+  const handleInputChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    const name = e.target.name.value.trim()
+    // update the phonebook
+    setPersons(persons.concat(name))
+    // update the name
+    setName('')
+  }
+
   return (
     <div>
-      <p> Hello </p>
+      <h1>Phonebook</h1>
+        <form onSubmit={handleFormSubmit}>
+          <div>
+            name: <input id="name" value={name} onChange={handleInputChange} />
+          </div>
+          <button type='submit'>add</button>
+        </form>
+      <h1>Numbers</h1>
+      <div>
+        {persons.map(
+          person => <p key={person}>{person}</p>
+        )}
+      </div>
     </div>
   )
 }
