59e1ae81b03cec6743cc0c4fa22d849d60076dc2
goofyangel <aleemertariq@gmail.com>
2025-04-14 10:53:24 -0700

    About to refactor.

--- milestone3/phonebook/src/App.jsx
+++ milestone3/phonebook/src/App.jsx
@@ -1,6 +1,17 @@
 import { useState } from 'react'
 
 const App = () => {
+
+  /**
+   * STATE FOR NEXT PARTS
+   */
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  // ])
+
   const [ persons, setPersons ] = useState([
     'Arto Hellas',
     'Ada Lovelace',
