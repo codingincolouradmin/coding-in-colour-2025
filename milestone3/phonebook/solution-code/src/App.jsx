64f0311494f5c10b2cd6fe10558a93d9e0f6a6f9
goofyangel <aleemertariq@gmail.com>
2025-04-14 14:59:10 -0700

    Finished feature implemnetations. Can do refactoring.

--- milestone3/phonebook/solution-code/src/App.jsx
+++ milestone3/phonebook/solution-code/src/App.jsx
@@ -1,41 +1,60 @@
 import { useState } from 'react'
 
+const Person = ({ name, number }) => {
+  return (
+    <div>
+      {name} {number}
+    </div>
+  )
+}
+
 const App = () => {
-
-  /**
-   * STATE FOR NEXT PARTS
-   */
-  // const [persons, setPersons] = useState([
-  //   { name: 'Arto Hellas', number: '040-123456', id: 1 },
-  //   { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-  //   { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-  //   { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  // ])
-
   const [ persons, setPersons ] = useState([
-    'Arto Hellas',
-    'Ada Lovelace',
-    'Dan Abramov',
-    'Mary Poppendieck'
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+  const [ search, setSearch ] = useState('')
   const [ name, setName ] = useState('')
+  const [ number, setNumber ] = useState('')
 
-  const handleInputChange = (e) => {
+  const handleNameChange = (e) => {
     setName(e.target.value)
+  }
+  const handleNumberChange = (e) => {
+    setNumber(e.target.value)
+  }
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value)
   }
 
   const handleFormSubmit = (e) => {
     e.preventDefault()
     const name = e.target.name.value.trim()
+    const number = e.target.number.value.trim()
     // Check if person exists & issue warning
-    const find = persons.find(person => person === name)
+    const find = persons.find(person => person.name === name)
     if (find) {
-      // Alert case
-      window.alert(`${find} is already added to the phonebook`)
+      console.log('entered duplicate case')
+      // Check if number is different
+      const updateNum = find.number !== number
+      if (updateNum) {
+        if (window.confirm(`${name} already exists, update their number?`)) {
+          const newPersons = persons.map(
+            (person) => person.name === name ? {...person, number } : person 
+          )
+          setPersons(newPersons)
+          setName('')
+          setNumber('')
+        }
+      }
     } else {
       // add to phonebook
-      setPersons(persons.concat(name))
+      setPersons(persons.concat({ id: persons.length + 1, name, number }))
       setName('')
+      setNumber('')
     }
     
   }
@@ -43,16 +62,21 @@ const App = () => {
   return (
     <div>
       <h1>Phonebook</h1>
+        <div>
+          filter shown with <input value={search} onChange={handleSearchChange} />
+        </div>
+      <h1>add a new</h1>
         <form onSubmit={handleFormSubmit}>
-          <div>
-            name: <input id="name" value={name} onChange={handleInputChange} />
-          </div>
+          <div> name: <input id="name" value={name} onChange={handleNameChange} /> </div>
+          <div> number: <input id="number" value={number} onChange={handleNumberChange} /> </div>
           <button type='submit'>add</button>
         </form>
       <h1>Numbers</h1>
       <div>
-        {persons.map(
-          person => <p key={person}>{person}</p>
+        {persons
+        .filter(p => p.name.toLowerCase().includes(search.toLowerCase()))
+        .map(
+          person => <Person key={person.id} name={person.name} number={person.number}/>
         )}
       </div>
     </div>
