870cc619c89208c9ba1943c5f704ef6958dc2f9a
goofyangel <aleemertariq@gmail.com>
2025-04-17 09:51:36 -0700

    Finished simple example.

--- milestone3/useEffects/starter/src/App.jsx
+++ milestone3/useEffects/starter/src/App.jsx
@@ -1,9 +1,30 @@
 import { useState, useEffect } from 'react'
 
 const App = () => {
+  const [ count, setCount ] = useState(0)
+  const [ flag, setFlag ] = useState(false)
+
+  useEffect(() => {
+    console.log(`The count is: ${count}`)
+  }, [count])
+
+  const handleClick = () => {
+    console.log('clicked...')
+    setFlag(!flag)
+  }
+
   return (
     <div>
-
+      <h1> Count: {count} </h1>
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <button onClick={handleClick}>
+        Click
+      </button>
     </div>
   )
 }
