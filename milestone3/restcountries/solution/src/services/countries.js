046083ded996f9c416dd393cce35187ef91b5258
goofyangel <aleemertariq@gmail.com>
2025-04-21 14:43:00 -0700

    Processed our Data, passed it back to the App.

--- milestone3/restcountries/solution/src/services/countries.js
+++ milestone3/restcountries/solution/src/services/countries.js
@@ -1,9 +1,23 @@
 import axios from 'axios'
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api'
 
+const processCountries = (rawData) => {
+  const processed = rawData.map(item => (
+    {
+      name: item.name?.common || "Unknown",
+      capital: item.capital?.[0] || "No capital",
+      area: item.area || 0,
+      languages: item.languages ? Object.values(item.languages) : "None",
+      flag: item.flags || None,
+      latlng: item.latlng || [0, 0]
+    }
+  ))
+  return processed;
+}
+
 const getAll = () => {
   const request = axios.get(`${baseUrl}/all`)
-  return request.then(response => response.data)
+  return request.then(response => processCountries(response.data))
 }
 
 export default { getAll } 
