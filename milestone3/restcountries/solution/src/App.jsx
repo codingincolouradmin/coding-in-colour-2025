60e9eb9a583399345241de2caa1176d7e602f20d
goofyangel <aleemertariq@gmail.com>
2025-04-21 14:30:56 -0700

    Initial setup, we are able to get our data with GET.

--- milestone3/restcountries/solution/src/App.jsx
+++ milestone3/restcountries/solution/src/App.jsx
@@ -1,4 +1,18 @@
+// React imports 
+import { useState, useEffect } from 'react'
+// Module imports 
+import countryServices from './services/countries'
+import countries from './services/countries'
+
 const App = () => {
+
+  // API request and countries update
+  useEffect(() => {
+    countryServices
+      .getAll()
+      .then(countries => console.log(countries))
+  }, [])
+
   return (
     <div>
       <p> Hello </p>
