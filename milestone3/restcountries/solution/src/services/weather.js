12e3e0c749f3ee1352a399cb0624acab55cb9ff1
goofyangel <aleemertariq@gmail.com>
2025-04-21 15:49:00 -0700

    Instructions added for API key.

--- milestone3/restcountries/solution/src/services/weather.js
+++ milestone3/restcountries/solution/src/services/weather.js
@@ -2,6 +2,13 @@ import axios from 'axios'
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather'
 const apiKey = import.meta.env.VITE_API_KEY
 
+/**
+ * Instructions
+ *  - Make an account on openweather
+ *  - Get your API key
+ *  - Add a .env with VITE_API_KEY = "..."
+ */
+
 const processWeather = (rawData) => {
   return {
     temperature: Math.round(rawData.main.temp - 273.15, 2),
